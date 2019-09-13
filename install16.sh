1cf1ec69d501a3dd28a82fae70cd5e40f12781b1
Laukik Hase <laukik17@gmail.com>
2019-09-13 20:16:51 +0530

    Update install16.sh

--- install16.sh
+++ install16.sh
@@ -1,3 +1,14 @@
+#Install ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+source /opt/ros/kinetic/setup.bash
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
 #Update Keys
 
 sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
